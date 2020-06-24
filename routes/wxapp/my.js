b97cf26c0371737776fe393d42795eeae7131271
RexJoush <joush970918@sina.com>
2020-06-24 17:39:36 +0800

    v1.1.2

---
+++ routes/wxapp/my.js
@@ -0,0 +1,13 @@
+
+/*
+    小程序端发现页的 路由
+ */
+let express = require("express");
+let router = express.Router();
+
+router.get('/',(req,res)=>{
+    console.log(req.query);
+    res.send("hello world");
+})
+
+module.exports = router;
