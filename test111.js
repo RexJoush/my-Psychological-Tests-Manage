50228887f7103bb6a5092c916c44ab2bd8f85938
RexJoush <joush970918@sina.com>
2020-07-03 17:48:31 +0800

    后台首页

--- test111.js
+++ test111.js
@@ -6,4 +6,25 @@ let sql = "SELECT" +
     " test_id = ?";
 
 
-console.log(sql);
+// console.log(sql);
+
+let utility = require("utility");
+
+let a = "111111";
+let b = "akdsjfhjksdfhjakshfjkba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad";
+
+// console.log(utility.md5(a));
+
+// a9993e364706816aba3e25717850c26c9cd0d89d
+// console.log(utility.sha1(a));
+// console.log(utility.sha1(b));
+
+let mysql = require("./util/mysql");
+
+// mysql.connection.query("select password from psy_user where username = 'admin'",(err, result)=>{
+//
+//     // console.log(result[0].password === utility.sha1(a));
+//     console.log(result);
+// });
+
+console.log((!(~+[])+{})[--[~+""][+[]]*[~+[]]+~~!+[]]+({}+[])[[~!+[]]*~+[]])
